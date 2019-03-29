a4486078594a5263e43fbd02a34ab939deead8f1
privateVoidMethod <kasperjohnsen@live.dk>
2019-03-29 14:50:32 +0100

    initial commit

---
+++ Chat/Models/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Models
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            var m = new Message { User = user, UserMessage = message };
+          
+            await Clients.Others.SendAsync("ReceiveMessage", m);
+
+        }
+    }
+
+}
---
+++ Chat/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Models
+{
+    public class Message
+    {
+        public string User { get; set; }
+        public string UserMessage { get; set; }
+
+    }
+}
